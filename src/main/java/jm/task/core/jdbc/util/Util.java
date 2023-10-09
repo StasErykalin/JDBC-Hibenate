a26bc99919872e229e69ea010c74b160617a780b
StasErykalin <stas@erykalin.ru>
2023-10-10 00:43:21 +0500

    Выполнил задачу 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,13 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Session getSessionFactory() {
+        SessionFactory sessionFactory = new Configuration().addAnnotatedClass(User.class).buildSessionFactory();
+        return sessionFactory.openSession();
+    }
 }
